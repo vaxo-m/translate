99b0643bcf4ecced422a22175c36df35d72837e3
sashkevichi <kakhidze2012@gmail.com>
2014-10-13 20:26:29 +0400

    unique words

--- routes/insert.js
+++ routes/insert.js
@@ -3,11 +3,7 @@ var wordSchema=require("./dbStart").Schema;
 var model=mongoose.model("words",wordSchema);
 
 module.exports.insert=function(word,def) {
-	var myWord=new model({name:word,explain:def});
-	myWord.save(function(err,data){
-		if(err){
-			console.log("word not saved");
-		}
-		console.log(data);
+	model.update({name:word},{explain:def},{upsert:true},function(err,data){
+		console.log("inserted");
 	});
 }
