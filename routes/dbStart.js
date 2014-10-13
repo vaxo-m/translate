40138aca42c6fd2fd895df735fa2f084c33eba09
sashkevichi <kakhidze2012@gmail.com>
2014-10-13 20:47:35 +0400

    add index

--- routes/dbStart.js
+++ routes/dbStart.js
@@ -1,7 +1,7 @@
 var mongoose=require("mongoose");
 var Schema=mongoose.Schema;
 var wordSchema=new Schema({
-	name:String,
+	name:{type:String , index: {unique: true} },
 	explain:String
 });
 module.exports.Schema=wordSchema;
